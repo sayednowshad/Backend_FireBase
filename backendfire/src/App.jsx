1b5c8ec024853c20adb6ca30b036535b2c8f59cc
Sayed Nowshad <sayednowshad58@gmail.com>
2025-02-26 12:04:51 +0530

    2 FA bug

--- backendfire/src/App.jsx
+++ backendfire/src/App.jsx
@@ -1,14 +1,16 @@
 import { getAuth, onAuthStateChanged } from "firebase/auth";
-import { app } from "./firebase";
 import { useEffect, useState } from "react";
-import { handleLogout } from "./auth"; // Import logout function
-import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import { app } from "./firebase";
 import Signup from "./Pages/Signup";
 import Signin from "./Pages/Signin";
+import React from "react";
+import "./App.css";
 import Navbar from "./Components/Navbar";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import Explore from "./Components/Explore";
 import Home from "./Components/Home";
 import About from "./Components/About";
+import { handleLogout } from "./auth"; // Import the logout function
 
 const auth = getAuth(app);
 
@@ -22,30 +24,37 @@ const App = () => {
       setLoading(false);
     });
 
-    return () => unsubscribe();
+    return () => unsubscribe(); // Cleanup function
   }, []);
 
   if (loading) {
-    return <p>Loading, please wait...</p>;
+    return (
+      <div className="loading-container">
+        <div className="spinner"></div>
+        <p>Loading, please wait...</p>
+      </div>
+    );
   }
 
   return (
     <Router>
-      {user && <Navbar auth={auth} onLogout={handleLogout} />} {/* Logout removes session */}
+      {user ? <Navbar auth={auth} onLogout={handleLogout} /> : null}
 
       <Routes>
-        {!user ? (
-          <>
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/signin" element={<Signin />} />
-            <Route path="*" element={<Navigate to="/signin" replace />} />
-          </>
-        ) : (
+        {/* Protected Routes - Only logged-in users can access */}
+        {user ? (
           <>
             <Route path="/explore" element={<Explore />} />
             <Route path="/home" element={<Home />} />
             <Route path="/about" element={<About />} />
-            <Route path="*" element={<Navigate to="/home" replace />} />
+          </>
+        ) : (
+          <>
+            {/* Show Signup and Signin when not logged in */}
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/signin" element={<Signin />} />
+            {/* Redirect unauthenticated users to Signin */}
+            <Route path="*" element={<Navigate to="/signin" replace />} />
           </>
         )}
       </Routes>
